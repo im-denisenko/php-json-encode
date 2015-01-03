a3aed9745a0008ec685e85be03d28a656e668a72
Igor Denisenko <im.minttea@gmail.com>
2015-01-04 02:51:43 +0300

    Remove decoder

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -5,11 +5,6 @@ defined('JSON_NUMERIC_CHECK') or define('JSON_NUMERIC_CHECK', 32);
 defined('JSON_UNESCAPED_SLASHES') or define('JSON_UNESCAPED_SLASHES', 64);
 defined('JSON_UNESCAPED_UNICODE') or define('JSON_UNESCAPED_UNICODE', 256);
 
-function future_json_decode()
-{
-    return call_user_func_array('Future\\Json::decode', func_get_args());
-}
-
 function future_json_encode()
 {
     return call_user_func_array('Future\\Json::encode', func_get_args());
